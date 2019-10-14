e1e011e3d9c8033120bfa9a21eb529faa61aa5a6
Reza Fer <hr.ferdowsi@gmail.com>
2019-10-14 21:02:49 +1100

    first commit

--- app.js
+++ app.js
@@ -47,9 +47,21 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // seting the mongoose on the database
+
+let url =process.env.DATABASEURL||"mongodb://localhost:27017/yelp_camp";
 // mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
-mongoose.connect("mongodb+srv://hrferdowsi:cFX2Xy1qnD8QOXh3@cluster0-glrab.mongodb.net/test?retryWrites=true&w=majority", { 
+// mongoose.connect("mongodb+srv://hrferdowsi:cFX2Xy1qnD8QOXh3@cluster0-glrab.mongodb.net/test?retryWrites=true&w=majority", { 
+//     useNewUrlParser: true,
+//     useCreateIndex:true 
+// }).then(()=>{
+//     console.log('connected to DB');
+// }).catch(err =>{
+//     console.log(`Error: ${err.message}`);
+// });
+
+
+mongoose.connect(url, { 
     useNewUrlParser: true,
     useCreateIndex:true 
 }).then(()=>{
